b447418b3301da78594ac4b02dbaa00a7e01ecf8
k1au3 <lindquistconcern@gmail.com>
2023-06-02 17:28:47 +0300

    App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 
 import './/styles/app.css';
+import Footer from './components/footer';
 import Hero from './components/hero';
 import Philosophy from './components/philosophy';
+import Products from './components/products';
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <Philosophy />
-      
+      <Products />
+      <Footer />
     </div>
   );
 }
